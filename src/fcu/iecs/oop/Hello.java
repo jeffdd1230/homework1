46f40e955beaedb3ff1d3d2fe6cf91122045d134
user <user@FE08>
2020-03-17 11:32:49 +0800

    second change

--- src/fcu/iecs/oop/Hello.java
+++ src/fcu/iecs/oop/Hello.java
@@ -4,7 +4,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String lyrics = "Let her go!Let it go! Can't hold it back anymore";
+		System.out.println(lyrics);
 	}
 
 }
